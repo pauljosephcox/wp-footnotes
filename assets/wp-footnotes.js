7fc94a847e0e2baa9a3e7c67d348722f2d9af299
Paul Cox <pauljosephcox@gmail.com>
2020-09-21 15:44:27 +1000

    Display block when open

--- assets/wp-footnotes.js
+++ assets/wp-footnotes.js
@@ -18,12 +18,15 @@ jQuery(document).ready(function($) {
         e.preventDefault(); 
         var $trigger = $(this);
         var $body = $trigger.next();
+        var $note = $trigger.parent();
 
         if($body.hasClass('open')){
             $body.removeClass('open');
+            $note.removeClass('open');
             $trigger.html('['+$trigger.attr('data-footnote-number')+']');
         } else {
             $body.addClass('open');
+            $note.addClass('open');
             $trigger.html('[x]');
         }
         
